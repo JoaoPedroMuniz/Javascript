ebec32b92c8b3bafb7fa03c50b322360886a6ed9
João Pedro <joaopedromuniz56@gmail.com>
2023-07-19 11:08:01 -0300

    exercícios

---
+++ cursoemvideo/aula04/ex008.js
@@ -0,0 +1,10 @@
+let vel = 67
+
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+
+if (vel >= 70){
+    console.log(`Você ultrapassou a velocidade permitida. Multado!`)
+}
+
+console.log('Dirija sempre usando cinto de segurança!')
+
---
+++ cursoemvideo/aula04/ex009.js
@@ -0,0 +1,9 @@
+var país = 'Brasil'
+
+console.log(`Vivendo no Brasil sendo do ${país} você é`)
+
+if (país != 'Brasil'){
+    console.log(`Estrangeiro`)
+} else{
+    console.log('Brasileiro')
+}
